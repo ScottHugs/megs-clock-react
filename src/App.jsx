acb6322368c79367d75f8a0e5f0be5f0f8b26a27
Scott Hughes <scott_t_hughes@outlook.com>
2024-03-05 09:56:58 +1100

    socket io working

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,41 @@
 import { useEffect, useState } from "react"
 import './App.css'
 import TimerDisplay from './Timer'
+import io from 'socket.io-client'
+
+const socket = io.connect('http://localhost:3365')
 
 function App() {
 
-  const [time, setTime] = useState(15*1000);
+  const [time, setTime] = useState(0);
+
+  const [timer, setTimer] = useState(0)
 
   useEffect(() => {
-    if (time !== 0) {
-      setTimeout(() => {
-        setTime(time - 1000); 
-      }, 1000)
-    }
-  }, [time])
+    socket.on('recieve_time', (data) => {
+      setTime(data)
+    })
+  }, [socket])
+
+  // useEffect(() => {
+  //   if (time !== 0) {
+  //     setTimeout(() => {
+  //       setTime(time - 1000); 
+  //     }, 1000)
+  //   }
+  // }, [time])
+
+  function startTimer(){
+    socket.emit('start_timer', 20000)
+  } 
 
   return (
-    <TimerDisplay remainingMilliseconds={time}/>
+    <section className="app">
+      {/* <TimerDisplay remainingMilliseconds={time}/> */}
+      <TimerDisplay remainingMilliseconds={time}/>
+      <button onClick={startTimer}>start timer</button>
+    </section>
+
   )
 }
 
